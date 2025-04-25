77eac18481958fb981fa668844be964b98c0b1f6
seray <serseray2003@gmail.com>
2025-04-25 09:54:53 +0300

    postgresqle taşındı

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -90,10 +90,14 @@ WSGI_APPLICATION = 'weather_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'weatherdb',
+        'USER': 'weatheruser',
+        'PASSWORD': '1963',
+        'HOST': 'localhost',
+        'PORT': '5433',
+}
 }
 
 
